e6a759020207d85b1b8710adee2b0bc825882509
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-06-09 22:55:07 +0530

    Time: 0 ms (100%), Space: 40.4 MB (93.88%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,12 +1,12 @@
 public class Solution {
-   // Function to count number of 1 b its
+
     public int hammingWeight(int n) {
         int count = 0;
 
-        while (n != 0) {
-            n = n & (n - 1); 
-            count++;
-        }
+       while(n!=0){
+        n=n&(n-1);
+        count++;
+       }
 
         return count;
     }

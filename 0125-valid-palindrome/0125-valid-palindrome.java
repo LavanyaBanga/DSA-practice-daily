cf3ca3cab4977e405871902b4dd8d2d9149e6db2
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-06-10 17:58:47 +0530

    Time: 4 ms (55.29%), Space: 44 MB (48.06%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       StringBuilder str=new StringBuilder();
+     for(char ch:s.toCharArray()){
+        if(Character.isLetterOrDigit(ch)){
+            str.append(Character.toLowerCase(ch));
+        }
+     }
+     return str.toString().equals(str.reverse().toString());
+
+       }
+    }
+

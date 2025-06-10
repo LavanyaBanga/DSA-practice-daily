f836d8423b374c1a32a09c472189afd33ff4c372
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-06-10 15:15:01 +0530

    Time: 0 ms (100%), Space: 41.8 MB (29.45%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -1,13 +1,12 @@
 class Solution {
     public char repeatedCharacter(String s) {
-      boolean[]alpha=new boolean[26];
-      for(char c:s.toCharArray()){
+     boolean alpha[]=new boolean[26];
+     for(char c:s.toCharArray()){
         if(alpha[c-'a']){
             return c;
-      }
-      alpha[c-'a']=true;
-     
-      }
-       return ' ';
+        }
+        alpha[c-'a']=true;
+     }
+     return ' ';
     }
 }

fbcd71212989ecfa58a17cfacfd4060e649e3a29
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-07-16 23:15:39 +0530

    Time: 7 ms (86.63%), Space: 45.3 MB (13.39%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,40 +1,36 @@
-import java.util.Stack;
-
 class Solution {
     public int calculate(String s) {
-        int length = s.length();
-        int sign = 1;
-        int ans = 0;
-        int currNo = 0;
         Stack<Integer> stack = new Stack<>();
-        
-        for (int i = 0; i < length; i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                currNo = s.charAt(i) - '0';
-                while (i + 1 < length && Character.isDigit(s.charAt(i + 1))) {
-                    currNo = currNo * 10 + s.charAt(i + 1) - '0';
-                    i++;
-                }
-                currNo = currNo * sign;
-                ans += currNo;
-                currNo = 0;
+        int number = 0, sign = 1, result = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                number = number * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
                 sign = 1;
-            } else if (s.charAt(i) == '+') {
-                sign = 1;
-            } else if (s.charAt(i) == '-') {
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
                 sign = -1;
-            } else if (s.charAt(i) == '(') {
-                stack.push(ans);
+            } else if (c == '(') {
+                stack.push(result);
                 stack.push(sign);
-                ans = 0;
                 sign = 1;
-            } else if (s.charAt(i) == ')') {
-                int prevSign = stack.pop();
-                ans = prevSign * ans;
-                int precAns = stack.pop();
-                ans = precAns + ans;
+                result = 0;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop(); 
+                result += stack.pop(); 
             }
         }
-        return ans;
+
+        if (number != 0) {
+            result += sign * number;
+        }
+
+        return result;
     }
 }

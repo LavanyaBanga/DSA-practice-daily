aed6930c7fa188725f366a84d10c57901a91dff7
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-07-16 23:15:05 +0530

    Time: 10 ms (37.76%), Space: 43.5 MB (82.76%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,34 +1,40 @@
+import java.util.Stack;
+
 class Solution {
     public int calculate(String s) {
-         int len = s.length(), sign = 1, result = 0;
+        int length = s.length();
+        int sign = 1;
+        int ans = 0;
+        int currNo = 0;
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
-
-            if (Character.isDigit(ch)) {
-                int num = ch - '0';
-                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
-                    num = num * 10 + s.charAt(++i) - '0';
+        
+        for (int i = 0; i < length; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                currNo = s.charAt(i) - '0';
+                while (i + 1 < length && Character.isDigit(s.charAt(i + 1))) {
+                    currNo = currNo * 10 + s.charAt(i + 1) - '0';
+                    i++;
                 }
-                result += num * sign;
-
-            } else if (ch == '+') {
+                currNo = currNo * sign;
+                ans += currNo;
+                currNo = 0;
                 sign = 1;
-            } else if (ch == '-') {
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
                 sign = -1;
-            } else if (ch == '(') {
-                stack.push(result);
+            } else if (s.charAt(i) == '(') {
+                stack.push(ans);
                 stack.push(sign);
-                result = 0;
+                ans = 0;
                 sign = 1;
-            } else if (ch == ')') {
-                result = result * stack.pop() + stack.pop();
+            } else if (s.charAt(i) == ')') {
+                int prevSign = stack.pop();
+                ans = prevSign * ans;
+                int precAns = stack.pop();
+                ans = precAns + ans;
             }
         }
-        return result;
+        return ans;
     }
 }
-
-        
-   

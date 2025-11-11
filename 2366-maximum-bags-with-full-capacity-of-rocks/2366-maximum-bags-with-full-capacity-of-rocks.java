3f25962f5c10f9fe1a8b41b2ae9885341e4ac029
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-11-11 16:55:35 +0530

    Time: 17 ms (30.17%), Space: 67.4 MB (5%) - LeetHub

--- 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.java
+++ 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,28 +1,21 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        int n = capacity.length;
-
-        // Calculate remaining capacities
-        for(int i = 0; i < n; i++) {
-            capacity[i] -= rocks[i];
-        }
-
-        // Sort remaining capacities
-        Arrays.sort(capacity);
-
-        // Determine maximum number of bags that could have full capacity
-        int maxBags = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(additionalRocks >= capacity[i]) {
-                additionalRocks -= capacity[i];
-                maxBags++;
-            } else {
-                break;
-            }
-        }
-        
-        return maxBags;
+    int n=capacity.length;
+    for(int i=0;i<n;i++){
+        capacity[i]-=rocks[i];
+      }
+      Arrays.sort(capacity);
+      int maxBags=0;
+    for(int i=0;i<n;i++){
+        if(additionalRocks>=capacity[i]){
+            additionalRocks-=capacity[i];
+            maxBags++;
+      }
+        else{
+            break;
+      }
+       }
+       return maxBags;
 
     }
 }

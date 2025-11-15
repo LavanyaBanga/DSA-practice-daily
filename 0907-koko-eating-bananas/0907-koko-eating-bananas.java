ca1ea47b5bc8b2460c053b138566a09039f3ee31
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-11-15 17:21:34 +0530

    Time: 8 ms (78.09%), Space: 47.8 MB (13.9%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,26 +1,27 @@
+
+
 class Solution {
     public int calculate(int[] piles, int mid) {
-      int hrs=0;
-      for(int t:piles){
-       hrs += (t + mid - 1) / mid;//works like ceil,ceil takes higher memory // ceil(double(piles)/mid)
+        int hrs = 0;
+        for (int t : piles) {
+            hrs += (t + mid - 1) / mid;  
         }
-
         return hrs;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
 
         int left = 1;
-        int right = Arrays.stream(piles).max().getAsInt(); 
+        int right = Arrays.stream(piles).max().getAsInt();
+
         while (left < right) {
             int mid = left + (right - left) / 2;
 
             int time = calculate(piles, mid);
 
             if (time <= h) {
-                right = mid;      
+                right = mid;    
             } else {
-                left = mid + 1;   
+                left = mid + 1; 
             }
         }
 

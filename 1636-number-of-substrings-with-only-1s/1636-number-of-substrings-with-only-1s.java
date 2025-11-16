d44f31e938b09dd5179949f34cdb66d5d6b1e639
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-11-16 21:09:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -1,14 +1,15 @@
 class Solution {
     public int numSub(String s) {
         long cnt = 0, total = 0, mod = 1000000007;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
-                cnt++;
-            } else {
-                cnt = 0;
-            }
-            total = (total + cnt) % mod;
+      for(int i=0;i<s.length;i++){
+        if(s.charAt(i)=='1'){
+            cnt++;
         }
-        return (int) total;
+        else{
+            cnt=0;
+        }
+      }
+      total=total+cnt%mod;
+      return total;
     }
 }

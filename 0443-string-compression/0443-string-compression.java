716b3b65d535913e971d9d0a5529ea9fbebd5773
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2025-12-22 17:12:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,11 +1,12 @@
 class Solution {
     public int compress(char[] chars) {
-      int i=0;
-      int index=0;
-      int n=chars.length;
-      while(i<n){
-        char current=chars[i];
+     int i=0;
+     int index=0;
+     int n=chars.length;
+     while(i<n){
         int count=0;
+        int current=chars[i];
+        //count the duplicates
         while(i<n && chars[i]==current){
             count++;
             i++;
@@ -13,12 +14,16 @@ class Solution {
         chars[index++]=current;
         String current_count=String.valueOf(count);
         if(count>1){
-            for(char ch:current_count.toCharArray()){
+            for(char ch:current_count.toCharArray(i)){
                 chars[index++]=ch;
             }
         }
-        
-      }
-      return index;
+        return index;
+
+
+
+     }
+
+     
     }
 }

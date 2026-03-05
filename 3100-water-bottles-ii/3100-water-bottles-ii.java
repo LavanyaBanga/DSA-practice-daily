4a7993efdf71dcfd640c9d48571dcbd6885b4fc1
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-03-05 18:31:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (24.09%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.java
+++ 3100-water-bottles-ii/3100-water-bottles-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
         // * Step 1: Drink all - Now numBottles display number of empty bottles
         int drink = numBottles;
-        // Exchange while possible
         while (numExchange <= numBottles) {
             // * 2a: Add a bottle after exchange (drink now)
             ++drink;

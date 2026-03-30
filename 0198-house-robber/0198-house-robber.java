ba1340afd4bafca40857a3fe9e4d785ade677535
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-03-30 15:18:53 +0530

    Time: 0 ms (100%), Space: 42.7 MB (66.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,25 @@
 class Solution {
+    int[] dp;
     public int rob(int[] nums) {
-        int n = nums.length;
+        dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = Math.max(ans, helper(nums, i));
+        }    
+        return ans;
+    }
 
-        if (n == 1) {
-            return nums[0];
+    int helper(int[] nums, int i) {
+        if (dp[i] != -1) return dp[i];
+        
+        int curr = nums[i];
+        int maxFutureLoot = 0;
+    
+        for (int j = i + 2; j < nums.length; j++) {
+            maxFutureLoot = Math.max(maxFutureLoot, helper(nums, j));
         }
-
-        int[] dp = new int[n];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[n - 1];        
+        
+        return dp[i] = curr + maxFutureLoot; 
     }
 }

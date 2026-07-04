2188294349471fe94acbd8f30e03ab640862630f
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-07-04 16:55:42 +0530

    Time: 0 ms (100%), Space: 46.8 MB (50.78%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    private int count = 0;
+    private int res = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return res;
+    }
+
+    private void inorder(TreeNode node, int k) {
+        if (node == null)
+            return;
+
+        inorder(node.left, k);
+
+        count++;
+        if (count == k) {
+            res = node.val;
+            return;
+        }
+
+        inorder(node.right, k);
+    }
+}

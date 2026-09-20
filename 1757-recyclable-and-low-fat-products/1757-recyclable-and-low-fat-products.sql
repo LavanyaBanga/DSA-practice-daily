01c47813ee9794c50650ec4ec08a8b9d0212a69b
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-09-20 16:48:24 +0530

    Time: 598 ms (52.74%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 #primary key contains unique values
-SELECT product_id
-FROM Products
-WHERE low_fats='Y' AND
- recyclable='Y'
+SELECT product_id FROM Products
+WHERE low_fats='Y'
+AND recyclable='Y'
